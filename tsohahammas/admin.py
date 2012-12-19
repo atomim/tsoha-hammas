077351a0c7e5d0f7362b00326115000be5e9c3dd
Olavi Lintumäki <atomimalli@gmail.com>
2012-12-19 23:13:18 +0200

    group and sample models seem to work file download doesn't.

---
+++ tsohahammas/admin.py
@@ -0,0 +1,5 @@
+from tsohahammas.models import *
+from django.contrib import admin
+
+admin.site.register(Group)
+admin.site.register(Sample)
--- tsohahammas/models.py
+++ tsohahammas/models.py
@@ -1,20 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-def Group(models.Model):
+class Group(models.Model):
      name = models.CharField(max_length=100)
      description = models.TextField()
      hidden = models.BooleanField()
      date_created= models.DateTimeField()
-     creator = models.ForeignKey('User')
-     members = models.ManyToManyField('User')
+     creator = models.ForeignKey(User)
+     members = models.ManyToManyField(User,related_name='r_groups')
      def __unicode__(self):
         return self.name
 
-def Sample(models.Model):
-     sample_id= models.AutoField()
+class Sample(models.Model):
+     sample_id= models.AutoField(primary_key=True)
      date_added= models.DateTimeField()
-     picture = models.ImageField()
-     adder = models.ForeignKey('User')
-     group = models.ForeignKey('Group')
+     picture = models.ImageField(upload_to="images/")
+     adder = models.ForeignKey(User)
+     group = models.ForeignKey(Group)
      
-
